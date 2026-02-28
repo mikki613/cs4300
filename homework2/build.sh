433aed4dfd79da00f56716104ec4631f93ccd801
mikki613 <mikki613@users.noreply.github.com>
2026-02-28 08:53:59 +0000

    Fix 500 error

--- homework2/build.sh
+++ homework2/build.sh
@@ -4,8 +4,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-#  Run migrations on deploy
 python manage.py migrate --noinput
-
-#  Static files for whitenoise
 python manage.py collectstatic --noinput
--- homework2/movie_theater_booking/urls.py
+++ homework2/movie_theater_booking/urls.py
@@ -4,9 +4,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    #  This adds URL names: login, logout, password_change, etc.
+    # REQUIRED for {% url 'login' %} and {% url 'logout' %}
     path("accounts/", include("django.contrib.auth.urls")),
 
-    # Your app
     path("", include("bookings.urls")),
 ]
