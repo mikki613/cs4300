bb051f4acea8ad27fed50434826c08ea3b7af944
mikki613 <mikki613@users.noreply.github.com>
2026-03-01 02:53:39 +0000

    Run migrations on deploy

--- homework2/build.sh
+++ homework2/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip
 pip install -r requirements.txt
-
-python manage.py migrate --noinput
+python manage.py migrate
 python manage.py collectstatic --noinput
